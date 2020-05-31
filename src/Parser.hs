97ba4800b5c8b429446321746e1b9400a4dda11f
Ryan Mehri <ryan.mehri1@gmail.com>
2020-05-30 19:30:47 -0600

    Finish first implementation of parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,8 @@
 module Parser where
 
 import           Text.Parsec
+import qualified Text.Parsec.Token             as P
+import           Text.Parsec.Language           ( haskellDef )
 
 data JValue =
     JNull
@@ -19,4 +21,40 @@ jNullParser = JNull <$ string "null"
 jBoolParser :: Parser JValue
 jBoolParser = JBool True <$ string "true" <|> JBool False <$ string "false"
 
+jNumberParser :: Parser JValue
+jNumberParser = JNumber <$> number
+  where
+    number = (char '-' *> (negate <$> naturalOrFloat)) <|> naturalOrFloat
+    naturalOrFloat = try float <|> (fromIntegral <$> natural)
 
+jStringParser :: Parser JValue
+jStringParser = JString <$> stringLiteral
+
+jArrayParser :: Parser JValue
+jArrayParser = JArray <$> brackets (commaSep jValueParser)
+
+jObjectParser :: Parser JValue
+jObjectParser = JObject <$> braces (commaSep pairParser)
+  where
+    pairParser = (,) <$> (stringLiteral <* spaces <* colon) <*> jValueParser
+
+jValueParser :: Parser JValue
+jValueParser =
+    jNullParser
+        <|> jBoolParser
+        <|> jNumberParser
+        <|> jStringParser
+        <|> jArrayParser
+        <|> jObjectParser
+
+------------------
+
+lexer = P.makeTokenParser haskellDef
+float = P.float lexer
+natural = P.natural lexer
+stringLiteral = P.stringLiteral lexer
+symbol = P.symbol lexer
+brackets = P.brackets lexer
+commaSep = P.commaSep lexer
+braces = P.braces lexer
+colon = P.colon lexer
