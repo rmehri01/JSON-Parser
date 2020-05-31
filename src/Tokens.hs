6cd9340f50baab3ea9c49866c23599dfaa617307
Ryan Mehri <ryan.mehri1@gmail.com>
2020-05-31 13:57:45 -0600

    Finish tests

--- src/Tokens.hs
+++ src/Tokens.hs
@@ -4,13 +4,13 @@
 -- More information at: https://hackage.haskell.org/package/parsec/docs/Text-ParserCombinators-Parsec-Token.html
 module Tokens where
 
+import Text.Parsec
 import qualified Text.Parsec.Token             as P
 import           Text.Parsec.Language           ( haskellDef )
 
 lexer = P.makeTokenParser haskellDef
 float = P.float lexer
 natural = P.natural lexer
-stringLiteral = P.stringLiteral lexer
 symbol = P.symbol lexer
 brackets = P.brackets lexer
 commaSep = P.commaSep lexer
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -5,6 +5,8 @@ import           Test.Tasty.HUnit
 
 import           Parser
 import           Data.Char                      ( toLower )
+import           Data.Either                    ( isLeft )
+import           Data.List                      ( isInfixOf )
 import           System.Environment
 
 main :: IO ()
@@ -28,16 +30,58 @@ scProps = testGroup
 
 qcProps = testGroup
     "(checked by QuickCheck)"
-    [ QC.testProperty "any given double can be parsed correctly" $ \double ->
+    [ QC.testProperty "any input that is not 'null' is not parsed" $ \string ->
+        string /= "null" QC.==> isLeft (runJsonParser string jNullParser)
+    , QC.testProperty "any input that is not 'true' or 'false' is not parsed"
+        $ \string -> (string /= "true" && string /= "false")
+              QC.==> isLeft (runJsonParser string jBoolParser)
+    , QC.testProperty "any given double can be parsed correctly" $ \double ->
         runJsonParser (show double) jNumberParser == Right (JNumber double)
     , QC.testProperty "any integer can be parsed and is converted to a double"
         $ \int -> runJsonParser (show (int :: Integer)) jNumberParser
               == Right (JNumber (fromIntegral int))
+    , QC.testProperty
+            "any non-quoted string between can be parsed when put between quotes"
+        $ \string ->
+              not ("\"" `isInfixOf` string)
+                  QC.==> (  runJsonParser ("\"" ++ string ++ "\"") jStringParser
+                         == Right (JString string)
+                         )
     ]
 
 unitTests = testGroup
     "Unit tests"
-    [ testCase "lowercase null parsed correctly"
-      $   runJsonParser "null" jNullParser
-      @?= Right JNull
+    [ testCase "lowercase null parses correctly"
+    $   runJsonParser "null" jNullParser
+    @?= Right JNull
+    , testCase "array with any amount of whitespace parses correctly"
+    $   runJsonParser "[ 1  ,   true ,  null  ,\"hello\"]" jArrayParser
+    @?= Right (JArray [JNumber 1.0, JBool True, JNull, JString "hello"])
+    , testCase "array with missing bracket should fail"
+    $   isLeft (runJsonParser "1, true, null, \"hello\"]" jArrayParser)
+    @?= True
+    , testCase "array with missing comma should fail"
+    $   isLeft (runJsonParser "[1, true, null \"hello\"]" jArrayParser)
+    @?= True
+    , testCase "object with any amount of whitespace parses correctly"
+    $   runJsonParser "{  \"first\"   : 24  ,\"second\" :   null}" jObjectParser
+    @?= Right (JObject [("first", JNumber 24.0), ("second", JNull)])
+    , testCase "object with missing bracket should fail"
+    $   isLeft
+            (runJsonParser "  \"first\"   : 24  ,\"second\" :   null}"
+                           jObjectParser
+            )
+    @?= True
+    , testCase "object with missing comma should fail"
+    $   isLeft
+            (runJsonParser "{  \"first\"   : 24  \"second\" :   null}"
+                           jObjectParser
+            )
+    @?= True
+    , testCase "object with missing colon should fail"
+    $   isLeft
+            (runJsonParser "{  \"first\"   24 ,  \"second\" :   null}"
+                           jObjectParser
+            )
+    @?= True
     ]
