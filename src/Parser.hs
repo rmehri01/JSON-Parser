41de726ba1cee752bd9bb2629c3249ab5118fc1f
Ryan Mehri <ryan.mehri1@gmail.com>
2020-05-30 18:31:56 -0600

    Setup data and start on parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,22 @@
 module Parser where
 
+import           Text.Parsec
+
+data JValue =
+    JNull
+    | JBool Bool
+    | JNumber Double
+    | JString String
+    | JArray [JValue]
+    | JObject [(String, JValue)]
+    deriving (Show, Eq)
+
+type Parser a = Parsec String () a
+
+jNullParser :: Parser JValue
+jNullParser = JNull <$ string "null"
+
+jBoolParser :: Parser JValue
+jBoolParser = JBool True <$ string "true" <|> JBool False <$ string "false"
+
 
